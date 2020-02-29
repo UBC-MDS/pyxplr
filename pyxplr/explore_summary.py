b1b909958291bc075f3762a79a4a22db351f6ca8
Serg Pokrovskyy <neosoftlviv@gmail.com>
2020-02-29 03:27:47 -0800

    Functions placeholders with docstrings placeholders

---
+++ pyxplr/explore_summary.py
@@ -0,0 +1,28 @@
+def explore_summary(dataframe):
+    """
+    <Fill in description>
+
+    Arguments
+    ---------
+    dataframe : pandas.DataFrame
+        The target dataframe to explore
+
+    Returns
+    -------
+    type :
+        Return value description
+
+    Raises
+    ------
+    Error
+        Description
+
+    Notes
+    -----
+    (Additional notes to be filled during development)
+
+    Examples
+    --------
+
+    """
+    raise NotImplemented
--- pyxplr/pyxplr.py
+++ pyxplr/pyxplr.py
@@ -1,1 +1,6 @@
+# Just import the functions here so that anyone importing `pyxplr`
+# will automatically get access to them
 from explore_feature_map import explore_feature_map
+from explore_missing import explore_missing
+from explore_summary import explore_summary
+from explore_outliers import explore_outliers

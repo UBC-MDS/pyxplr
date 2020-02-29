6c3f63b6464379fb05a5e94c35aa2b104b723fe4
Braden Tam <btam97@gmail.com>
2020-02-28 20:32:26 -0800

    add prelim function

---
+++ pyxplr/get_na.py
@@ -0,0 +1,12 @@
+"""
+Created on February 28, 2020
+@author: Braden Tam
+Implementation of the get_na function in the pyxplr package.
+"""
+
+def get_na(data):
+    """
+    get_na will return 2 tables: 1 table of exactly where there is missing data 
+    and another table how much data is missing for each feature.
+
+    """
